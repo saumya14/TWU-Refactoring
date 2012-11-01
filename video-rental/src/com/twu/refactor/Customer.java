05ec2d44680e46eee07672f7982391c59d5c23cc
unknown <saumyasi@SAUMYAS.corporate.thoughtworks.com>
2012-11-01 20:04:57 +0530

    Moved the rental amount calculation to Rental class.

--- video-rental/src/com/twu/refactor/Customer.java
+++ video-rental/src/com/twu/refactor/Customer.java
@@ -3,7 +3,7 @@ package com.twu.refactor;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-//Understands the entity that would be renting out books.
+//Understands the entity that would be borrowing.
 public class Customer {
 
 	private String name;
@@ -29,30 +29,14 @@ public class Customer {
 		while (rentals.hasNext()) {
 			double thisAmount = 0;
 			Rental each = rentals.next();
+            thisAmount = each.calculateRentalAmount();
 
-			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
 
-			}
-
-			// add frequent renter points
+            // add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
 			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
+					&& each.daysRented() > 1)
 				frequentRenterPoints++;
 
 			// show figures for this rental
@@ -61,11 +45,9 @@ public class Customer {
 			totalAmount += thisAmount;
 
 		}
-		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
 	}
-
 }
--- video-rental/src/com/twu/refactor/Rental.java
+++ video-rental/src/com/twu/refactor/Rental.java
@@ -11,11 +11,33 @@ public class Rental {
         this.daysRented = daysRented;
     }
 
-    public int getDaysRented() {
+    public int daysRented() {
         return daysRented;
     }
 
     public Movie getMovie() {
         return movie;
+    }
+
+    public double calculateRentalAmount()
+    {
+        double rentalAmount=0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                rentalAmount += 2;
+                if (daysRented() > 2)
+                    rentalAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                rentalAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                rentalAmount += 1.5;
+                if (daysRented > 3)
+                    rentalAmount += (daysRented - 3) * 1.5;
+                break;
+
+        }
+        return rentalAmount;
     }
 }
